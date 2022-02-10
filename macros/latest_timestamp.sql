61ba211279eb5020515222f018049611e963c988
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-02-10 19:30:31 -0400

    fix latest_timestamp macro

--- macros/latest_timestamp.sql
+++ macros/latest_timestamp.sql
@@ -4,14 +4,20 @@
         timestamp_column,
         feature_columns=['*']
     ) %}
+WITH __f__latest_timestamp AS (
+    SELECT
+        *,
+        {{ entity_column }} AS __f__entity,
+        {{ timestamp_column }} AS __f__timestamp,
+        {{ next_timestamp(entity_column, timestamp_column) }} AS __f__next_timestamp
+    FROM {{ feature_table }}
+)
 SELECT
     {% for column in feature_columns %}
     {{ column }},
     {% endfor %}
-
-    {{ entity_column }} AS __f__entity,
-    {{ timestamp_column }} AS __f__timestamp,
-    {{ next_timestamp(entity_column, timestamp_column) }} AS __f__next_timestamp
-FROM {{ feature_table }}
+    __f__entity,
+    __f__timestamp
+FROM __f__latest_timestamp
 WHERE __f__next_timestamp IS NULL
 {% endmacro %}
